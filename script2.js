fc9aef20ee128f67ab885148965d87cbee41fb3e
alex <alex19mene@gmail.com>
2025-08-21 15:24:50 -0300

    atomica to atomica

--- script2.js
+++ script2.js
@@ -28,7 +28,7 @@ const sons = {
   default:   'assets/sons/default.mp3',
   verao: 'assets/sons/Brazilian-Sunset.mp3',
   inverno:   'assets/sons/Copacabana-Beach-Dreams.mp3',
-  primavera: 'assets/sons/Samba-Atômica.mp3'
+  primavera: 'assets/sons/Samba-Atomica.mp3'
 }
 
 function getContexto() {
